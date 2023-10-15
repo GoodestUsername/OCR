c99c80738617617318c44167c1f487b29d71e3e1
GoodestUsername <ericdongcannn@hotmail.com>
2023-10-15 16:30:09 -0700

    Implemented scrollable text element displaying scanned text. Added button to clear scanned text.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,79 @@
 import TextRecognition from '@react-native-ml-kit/text-recognition';
 import { Camera, CameraType, ImageType } from 'expo-camera';
-import { useRef, useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useMemo, useRef, useState } from 'react';
+import {
+	Button,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
 
-export default function App() {
+const App = () => {
 	const [type, setType] = useState(CameraType.back);
+	const [scannedText, setScannedText] = useState<string>('');
 	const [permission, requestPermission] = Camera.useCameraPermissions();
 	const cameraRef = useRef<Camera>(null);
+
+	const toggleCameraType = () => {
+		setType(current =>
+			current === CameraType.back ? CameraType.front : CameraType.back,
+		);
+	};
+
+	const takePicture = () => {
+		// TODO: implement upload via "react-native-background-upload
+		// pause > resume > takepicture is used to create illusion of speed of capture.
+		cameraRef.current?.pausePreview();
+		cameraRef.current?.resumePreview();
+		cameraRef.current?.takePictureAsync({
+			quality: 1,
+			base64: true,
+			exif: false,
+			imageType: ImageType.png,
+			skipProcessing: false,
+			onPictureSaved: newPhoto => {
+				TextRecognition.recognize(newPhoto.uri).then(result => {
+					setScannedText(result?.text);
+				});
+			},
+		});
+	};
+
+	const clearText = () => {
+		setScannedText('');
+	};
+
+	const ScannedTextSection = useMemo(() => {
+		return (
+			<ScrollView
+				showsVerticalScrollIndicator={true}
+				persistentScrollbar={true}
+				style={styles.buttonContainer}>
+				<Text selectable={true} style={[styles.scannedText]}>
+					{scannedText}
+				</Text>
+			</ScrollView>
+		);
+	}, [scannedText]);
+
+	const ButtonSection = useMemo(() => {
+		return (
+			<View style={styles.buttonContainer}>
+				<TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+					<Text style={styles.text}>Flip Camera</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={takePicture}>
+					<Text style={styles.text}>Take Picture</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.button} onPress={clearText}>
+					<Text style={styles.text}>Clear Text</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}, []);
 
 	if (!permission) {
 		// Camera permissions are still loading
@@ -25,64 +92,18 @@ export default function App() {
 		);
 	}
 
-	function toggleCameraType() {
-		setType(current =>
-			current === CameraType.back ? CameraType.front : CameraType.back,
-		);
-	}
-	function takePicture() {
-		// TODO: implement upload via "react-native-background-upload
-		const start = performance.now();
-		cameraRef.current?.pausePreview();
-		cameraRef.current?.resumePreview();
-		console.log('start ', start);
-		const options: any = {
-			forceUpOrientation: true,
-			fixOrientation: true,
-			quality: 1,
-			base64: true,
-			exif: false,
-			autoFocus: false,
-			imageType: ImageType.png,
-			skipProcessing: false,
-			onPictureSaved: (newPhoto: { uri: string; base64: string | any[] }) => {
-				const end = performance.now();
-				console.log('end ', end);
-				console.log(
-					`time elapsed: ${(end - start) * 0.001} seconds ${start}, ${end}`,
-				);
-				TextRecognition.recognize(newPhoto.uri).then(result => {
-					console.log('Recognized text:', result.text);
-
-					for (const block of result.blocks) {
-						console.log('Block text:', block.text);
-						console.log('Block frame:', block.frame);
-
-						for (const line of block.lines) {
-							console.log('Line text:', line.text);
-							console.log('Line frame:', line.frame);
-						}
-					}
-				});
-			},
-		};
-		cameraRef.current?.takePictureAsync(options);
-	}
 	return (
 		<View style={styles.container}>
+			<StatusBar />
 			<Camera style={styles.camera} type={type} ref={cameraRef}>
-				<View style={styles.buttonContainer}>
-					<TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-						<Text style={styles.text}>Flip Camera</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={takePicture}>
-						<Text style={styles.text}>Take picture</Text>
-					</TouchableOpacity>
-				</View>
+				{ScannedTextSection}
+				{ButtonSection}
 			</Camera>
 		</View>
 	);
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
 	container: {
@@ -93,10 +114,10 @@ const styles = StyleSheet.create({
 		flex: 1,
 	},
 	buttonContainer: {
-		flex: 1,
 		flexDirection: 'row',
 		backgroundColor: 'transparent',
-		margin: 64,
+		margin: 32,
+		marginTop: 0,
 	},
 	button: {
 		flex: 1,
@@ -106,6 +127,18 @@ const styles = StyleSheet.create({
 	text: {
 		fontSize: 24,
 		fontWeight: 'bold',
+		color: 'white',
+	},
+	scannedTextContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		backgroundColor: 'transparent',
+		margin: 16,
+		marginBottom: 0,
+	},
+	scannedText: {
+		flex: 1,
+		fontSize: 12,
 		color: 'white',
 	},
 });
